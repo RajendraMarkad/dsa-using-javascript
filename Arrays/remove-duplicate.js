c68c7f144746fffda1b1590ba57d443ddecd2bd5
RajendraMarkad <100752426+RajendraMarkad@users.noreply.github.com>
2024-02-21 18:50:08 +0530

    Create remove-duplicate.js

---
+++ Arrays/remove-duplicate.js
@@ -0,0 +1,47 @@
+const array = [1, 2, 3, 1, 2, 4, 5, 3];
+
+function removeDuplicates(arr) {
+    const uniqueArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        let isDuplicate = false;
+        // Check if the current element is already in the uniqueArray
+        for (let j = 0; j < uniqueArray.length; j++) {
+            if (arr[i] === uniqueArray[j]) {
+                isDuplicate = true;
+                break;
+            }
+        }
+        // If not a duplicate, add it to the uniqueArray
+        if (!isDuplicate) {
+            uniqueArray.push(arr[i]);
+        }
+    }
+    return uniqueArray;
+}
+
+const resultArray = removeDuplicates(array);
+console.log(resultArray); // Output: [1, 2, 3, 4, 5]
+
+
+# Optimised Way
+
+const array = [1, 2, 3, 1, 2, 4, 5, 3];
+
+function removeDuplicates(arr) {
+    const uniqueArray = [];
+    const encounteredElements = {};
+
+    for (let i = 0; i < arr.length; i++) {
+        const element = arr[i];
+        // Check if the current element has been encountered before
+        if (!encounteredElements[element]) {
+            uniqueArray.push(element);
+            encounteredElements[element] = true; // Mark the element as encountered
+        }
+    }
+
+    return uniqueArray;
+}
+
+const resultArray = removeDuplicates(array);
+console.log(resultArray); // Output: [1, 2, 3, 4, 5]
